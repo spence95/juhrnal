d24e057b6c3c57fc3ad12b5beaf759426bad1a29
Spencer Sutton <sutton1190@gmail.com>
2016-05-12 00:00:38 -0600

    styling updates, multiple tabs, history func

--- index.js
+++ index.js
@@ -9,19 +9,7 @@ Database entry table has the following fields
 */
 var Datastore = require('nedb')
  , db = new Datastore({ filename: 'C:/Shadowmere/learning/journalingapp/main.db'});
-var entriesShownLimit = 10;
 
-//get all documents
-db.loadDatabase(function(err){
-  if(!err){
-    db.find({username: 'spencer'})
-    .limit(entriesShownLimit).exec(function (err, entries) {
-      console.log(entries);
-    });
-  } else {
-    alert("Database retrieval error: " + err);
-  }
-});
 
 
 
@@ -49,3 +37,67 @@ document.getElementById("submitEntry").addEventListener("click", function (e) {
     }
   });
 });
+
+//new entry tab listener
+document.getElementsByClassName("sideButton")[0].addEventListener("click", function(e){
+  document.getElementById("newEntry").style.display = 'block';
+  document.getElementById("history").style.display = "none";
+  toggleSideButtons(0);
+});
+
+//history tab listener
+document.getElementsByClassName("sideButton")[1].addEventListener("click", function(e){
+  toggleSideButtons(1);
+
+  document.getElementById("newEntry").style.display = 'none';
+  loadHistory();
+});
+
+//settings tab listener
+document.getElementsByClassName("sideButton")[2].addEventListener("click", function(e){
+  toggleSideButtons(2);
+  document.getElementById("history").style.display = "none";
+  document.getElementById("newEntry").style.display = 'none';
+});
+
+function toggleSideButtons(activeIndex){
+  var i = 0;
+  var els = document.getElementsByClassName("sideButton");
+
+  Array.prototype.forEach.call(els, function(el) {
+      if(i == activeIndex){
+        el.className += " activeSB";
+      } else {
+        el.className = "sideButton";
+      }
+      i++;
+  });
+
+
+}
+
+
+function loadHistory(){
+  document.getElementById("history").style.display = 'block';
+
+  var entriesShownLimit = 10;
+
+  //get all documents
+  db.loadDatabase(function(err){
+    if(!err){
+      db.find({username: 'spencer'}).sort({ entryDate: -1 })
+      .limit(entriesShownLimit).exec(function (err, entries) {
+        //populate entries
+        document.getElementById('history').innerHTML = "";
+        entries.forEach(function(entry){
+          document.getElementById('history').innerHTML += "<div class='entry'> \
+            <div class='entryDate'>" + entry.entryDate + "</div> \
+            <div class='entryText'>" + entry.text + "</div> \
+          </div>";
+        });
+      });
+    } else {
+      alert("Database retrieval error: " + err);
+    }
+  });
+}
